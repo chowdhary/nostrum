8031662e3b54105b4d04a56c07eadf2905ef729b
Vibhor Chowdhary <vibhor.chowdhary@gmail.com>
2025-04-04 21:11:20 -0500

    updated gun package to 2.1

--- mix.exs
+++ mix.exs
@@ -177,7 +177,7 @@ defmodule Nostrum.Mixfile do
       {:jason, "~> 1.4"},
       # Replacement for Jason, remove once we required OTP 27+
       # {:json_polyfill, "~> 0.2"},
-      {:gun, "~> 2.0"},
+      {:gun, "~> 2.1"},
       {:salchicha, "~> 0.3"},
       {:certifi, "~> 2.13"},
       {:mime, "~> 1.6 or ~> 2.0"},
